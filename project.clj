e29da8d97a93be361d7304f4cbdc13bafe8a54ca
Yannick Scherer <yannick@xsc.dev>
2023-12-11 16:50:22 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -6,21 +6,23 @@
             :comment "MIT License"
             :year 2022
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
 
                  ;; Google
-                 [com.google.api-client/google-api-client "1.33.4"]
-                 [com.google.http-client/google-http-client "1.41.5"]
-                 [com.google.http-client/google-http-client-jackson2 "1.41.5"]
+                 [com.google.api-client/google-api-client "1.35.2"]
+                 [com.google.http-client/google-http-client "1.43.3"]
+                 [com.google.http-client/google-http-client-jackson2 "1.43.3"]
                  [com.google.apis/google-api-services-sheets "v4-rev12-1.22.0"]
-                 [com.google.oauth-client/google-oauth-client "1.33.1"]
-                 [com.google.oauth-client/google-oauth-client-jetty "1.33.1"]
+                 [com.google.oauth-client/google-oauth-client "1.34.1"]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.34.1"]
 
                  ;; Bunq
-                 [com.github.bunq/sdk_java "1.19.1.3"]
+                 [com.github.bunq/sdk_java "1.19.11.48"]
 
                  ;; Utilities
-                 [com.google.code.gson/gson "2.9.0"]
+                 [org.apache.httpcomponents/httpclient "4.5.14"]
+                 [org.apache.httpcomponents/httpcore "4.4.16"]
+                 [com.google.code.gson/gson "2.10.1"]
                  [aero "1.1.6"]]
   :repositories {"jitpack" {:url "https://jitpack.io"}}
   :main bunq-google-sheets-sync.core
